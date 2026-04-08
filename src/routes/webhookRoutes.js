fdb2ef3e616d3a3c830cd9c788c820165045c852
IllalRajinCoding <roxyuciha@gmail.com>
2026-04-08 14:41:00 +0700

    push

--- src/routes/webhookRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { handlePaymentWebhook } from "../controllers/paymentWebhookController.js";
-
-const router = express.Router();
-
-router.post("/payment", handlePaymentWebhook);
-
-export default router;
